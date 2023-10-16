bccf212c229f20e4adaa27565486330ce9b94634
jlio215 <31058099+jlio215@users.noreply.github.com>
2023-10-16 14:57:49 -0500

    Update URL

--- src/configuration/axiosConfiguration.js
+++ src/configuration/axiosConfiguration.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const axiosConfiguration = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL ||
-     'https://movies-back-end-0pxp.onrender.com/api/v1'
+     'https://movies-back-end-8uo6.onrender.com/api/v1'
 })
 
 export {
